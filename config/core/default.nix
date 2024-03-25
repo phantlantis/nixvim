4739dcf3d973589b1db773837d0ea316534e0f6b
Ben Tuttle <tuttle425@gmail.com>
2024-03-24 18:43:00 -0600

    moving options and adding neogen

--- config/core/default.nix
+++ config/core/default.nix
@@ -1,7 +1,17 @@
 {pkgs, ...}: {
   imports = [
     ./core.nix
-
+    ./neogen.nix
     ./treesitter.nix
   ];
+
+  globals.mapleader = " ";
+  clipboard.register = "unnamedplus";
+  
+  plugins = {
+    surround.enable = true;
+    comment-nvim.enable = true;
+
+  };
+
 }
--- config/options.nix
+++
@@ -1,6 +0,0 @@
-{
-    config.globals = {
-      mapleader = " ";
-    };
-   
-}
